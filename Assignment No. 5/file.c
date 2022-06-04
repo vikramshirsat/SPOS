4067e1c1ac18c2f153a7f9aea67f794a695094cf
Vikram Shirsat <95412686+vikramshirsat@users.noreply.github.com>
2022-06-04 22:42:25 +0530

    Add files via upload

---
+++ Assignment No. 5/file.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int c;
+	c = 9;
+	printf("The number is :",c);
+	//this is single line comment
+	/*
+	This is multiline comment
+	For practice
+	*/
+}
